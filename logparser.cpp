34e9dc919303d796b5b81bbd0e75e5d676035b66
Mahsum Saetgareev <mahssaet23@gmail.com>
2022-11-02 17:46:57 +0300

    regex filename, with progress bar

--- logparser.cpp
+++ logparser.cpp
@@ -11,6 +11,23 @@
 namespace fs = std::filesystem;
 using namespace std::literals;
 
+struct ParseInfo
+{
+	ParseInfo operator+(ParseInfo other)
+	{
+		return ParseInfo{lines_found + other.lines_found, files_found + other.files_found, files_scanned + other.files_scanned};
+	}
+	ParseInfo& operator+=(ParseInfo other)
+	{
+		*this = *this + other;
+		return *this;
+	}
+
+	int lines_found = 0;
+	int files_found = 0;
+	int files_scanned = 0;
+};
+
 int MeasureFile(std::ifstream& in)
 {
 	in.seekg(0, std::ios::end);
@@ -54,6 +71,45 @@ int SearchRegexInFile(std::ifstream& in, const std::regex& reg, std::ofstream& o
 	return cnt;
 }
 
+ParseInfo ParseLogInDir(const fs::path& dir, const std::regex& reg, std::ofstream& out)
+{
+	ParseInfo res;
+	std::cout << "Directory: " << dir.string() << std::endl;
+	for (const auto& file : fs::directory_iterator(dir))
+	{
+		std::ifstream input(file.path().string());
+		if (!input)
+		{
+			std::cerr << "Can't open file " << file.path().string() << std::endl; 
+			continue;
+		}
+		int cur_found = SearchRegexInFile(input, reg, out);
+		std::cout << '\n' << file << ": found " << cur_found << " matches\n"; 
+		res.lines_found += cur_found;
+		res.files_found += (cur_found > 0);
+		++res.files_scanned;
+	}
+	std::cout << std::endl;
+	return res;
+}
+
+ParseInfo ParseLogFile(const fs::path& file, const std::regex& reg, std::ofstream& out)
+{
+	ParseInfo res;
+	std::ifstream input(file.string());
+	if (!input)
+	{
+		std::cerr << "Can't open file " << file.string() << std::endl;
+		return res;
+	}
+	res.lines_found = SearchRegexInFile(input, reg, out);
+	res.files_found = (res.lines_found > 0);
+	res.files_scanned = 1;
+	std::cout << '\n' << file.string() << ": found " << res.lines_found << " matches\n";
+	return res;
+}
+
+
 int main(int argc, char* argv[])
 {
 	if (argc < 3)
@@ -78,40 +134,25 @@ int main(int argc, char* argv[])
 		return 2;
 	}
 
-	int lines_found = 0, files_found = 0, filenum = 0;
-	if (fs::is_directory(fs::path(argv[1])))
+	ParseInfo parse_results;
+	for (const auto& file : fs::directory_iterator(fs::current_path()))
 	{
-		for (const auto& file : fs::directory_iterator(fs::path(argv[1])))
+		if (std::regex_search(file.path().string(), std::regex(argv[1])))
 		{
-			std::ifstream input(file.path().string());
-			if (!input)
+			if (fs::is_directory(file.path()))
 			{
-				std::cerr << "Can't open file " << file << std::endl; 
-				continue;
+				parse_results += ParseLogInDir(file.path(), std::regex(argv[2]), output);
 			}
-			int cur_found = SearchRegexInFile(input, std::regex(argv[2]), output);
-			std::cout << '\n' << file << ": found " << cur_found << " matches\n"; 
-			lines_found += cur_found;
-			files_found += (cur_found > 0);
-			++filenum;
+			else
+			{
+				parse_results += ParseLogFile(file.path(), std::regex(argv[2]), output);
+			}
 		}
 	}
-	else
-	{
-		std::ifstream input(argv[1]);
-		if (!input)
-		{
-			std::cerr << "Can't open file " << argv[1] << std::endl;
-			return 2;
-		}
-		lines_found = SearchRegexInFile(input, std::regex(argv[2]), output);
-		if (lines_found > 0)
-			files_found = 1;
-		filenum = 1;
-	}
+
 	std::cout << std::endl;
-	std::cout << "Files scanned: " << filenum << std::endl
-		      << "Files matched: " << files_found << std::endl
-		      << "Lines matched: " << lines_found << std::endl;			  
+	std::cout << "Files scanned: " << parse_results.files_scanned << std::endl
+		      << "Files matched: " << parse_results.files_found << std::endl
+		      << "Lines matched: " << parse_results.lines_found << std::endl;			  
 	return 0;
 }
